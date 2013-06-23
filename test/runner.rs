0f172ac8db99bacaee53768095f45383c66a4db1
Fedor Indutny <fedor.indutny@gmail.com>
2013-06-23 22:00:07 +0200

    test: rex

--- test/runner.rs
+++ test/runner.rs
@@ -112,3 +112,30 @@ fn proc() {
     m.ret(Empty);
   };
 }
+
+#[test]
+fn rex() {
+  do run_test(0x1234, 0x1234) |m| {
+    m.pushq(R(rbp));
+    m.movq(R(rbp), R(rsp));
+
+    m.movq(R(r15), R(rsi));
+
+    m.movq(R(r14), Long(0x00ff));
+    m.movq(R(r13), Long(0xff00));
+    m.andq(R(r15), R(r14));
+    m.andq(R(rsi), R(r13));
+    m.addq(R(r15), R(rsi));
+
+    m.xchgq(R(rax), R(r15));
+
+    // Cleanup?!
+    m.xorq(R(r13), R(r13));
+    m.xorq(R(r14), R(r14));
+    m.xorq(R(r15), R(r15));
+
+    m.movq(R(rsp), R(rbp));
+    m.popq(R(rbp));
+    m.ret(Empty);
+  }
+}
